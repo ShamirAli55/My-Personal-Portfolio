0898208759157ebae200bdf866d6657cadab53f7
ShamirAli55 <shamirali9779@gmail.com>
2025-08-28 12:36:06 +0500

    Woff fromat Added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -33,27 +33,27 @@ const Banner = () => {
 
   return (
     <section className="min-h-[50vh] relative bg-muted w-full overflow-hidden flex items-center py-32">
-      <div className="slider h-1/2 bg-opposite text-primary-foreground py-4">
+      <div className="slider md:h-1/2 bg-opposite text-primary-foreground py-4 pointer-events-none">
         {[...Array(5)].map((_, i) => (
-          <div key={i} className="flex text-5xl gap-x-5 items-center">
+          <div
+            key={i}
+            className="flex text-lg md:text-5xl gap-x-5 items-center"
+          >
             <ArrowRight
-              size={60}
               color="hsl(var(--primary))"
-              className="bg-primary-foreground rounded-full"
+              className="h-12 w-12 md:h-14 md:w-14 bg-primary-foreground rounded-full"
               ref={(el) => (arrowsRef.current[i * 3] = el)}
             />
             <h4>Experience</h4>
             <ArrowRight
-              size={60}
               color="hsl(var(--primary))"
-              className="bg-primary-foreground rounded-full"
+              className="h-12 w-12 md:h-14 md:w-14 bg-primary-foreground rounded-full"
               ref={(el) => (arrowsRef.current[i * 3 + 1] = el)}
             />
             <h4>Content</h4>
             <ArrowRight
-              size={60}
               color="hsl(var(--primary))"
-              className="bg-primary-foreground rounded-full"
+              className=" h-12 w-12 md:h-14 md:w-14 bg-primary-foreground rounded-full"
               ref={(el) => (arrowsRef.current[i * 3 + 2] = el)}
             />
             <h4>Development</h4>
